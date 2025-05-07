7331cfabd20ddb46e542e453efc7c57a81200e34
SrNoelll <159236885+SrNoelll@users.noreply.github.com>
2025-05-07 19:19:53 +0200

    este si el ultimo

--- src/assets/components/LoginComponent/LoginComponent.jsx
+++ src/assets/components/LoginComponent/LoginComponent.jsx
@@ -15,7 +15,7 @@ const LoginComponent = () => {
         }
     }
     const cargarEjercicios = () =>{
-        fetch('https://api.allorigins.win/get?url=' + encodeURIComponent('https://goldsfit.infinityfreeapp.com/index.php'), {
+        fetch('https://proxy.cors.sh/https://goldsfit.infinityfreeapp.com/index.php', {
             method: "POST",
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
