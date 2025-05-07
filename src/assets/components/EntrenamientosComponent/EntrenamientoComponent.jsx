4ab8645409bef05d015ec5249ef2eed1c14448bc
SrNoelll <159236885+SrNoelll@users.noreply.github.com>
2025-05-07 16:52:19 +0200

    sesion?

--- src/assets/components/EntrenamientosComponent/EntrenamientoComponent.jsx
+++ src/assets/components/EntrenamientosComponent/EntrenamientoComponent.jsx
@@ -3,6 +3,10 @@ import HeaderComponent from '../HeaderComponent/HeaderComponent'
 import RutinasComponent from './RutinasComponent/RutinasComponent'
 
 const EntrenamientoComponent = () => {
+  if (localStorage.getItem("nombre") === null) {
+    window.location.href = "/login";
+  }
+  
   return (
     <div>
       <HeaderComponent/>
