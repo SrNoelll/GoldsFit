72a4005dbdc6c8f9854e19f8f3a660b3591fe885
SrNoelll <159236885+SrNoelll@users.noreply.github.com>
2025-05-19 10:23:46 +0200

    Update TecnologiasComponent.jsx

--- src/assets/components/IndexComponent/TecnologiasComponent/TecnologiasComponent.jsx
+++ src/assets/components/IndexComponent/TecnologiasComponent/TecnologiasComponent.jsx
@@ -8,7 +8,7 @@ const TecnologiasComponent = () => {
                 <div><h2 className='titulo'>Siempre con la ultima tecnologia</h2></div>
                 <div className='ct col-4 p-3'><p className=' '>Nuestra aplicación combina la experiencia de profesionales del fitness y la nutrición con las últimas tecnologías en inteligencia artificial. Analizamos tu progreso, adaptamos tus rutinas de entrenamiento y tu alimentación, y te proporcionamos recomendaciones personalizadas en tiempo real. Con nuestro sistema inteligente, llevar un estilo de vida saludable nunca ha sido tan fácil y eficiente.</p>
                   <div className='container-fluid d-flex justify-content-end'>
-                    <button>
+                    <button className='x'>
                       <span> EMPIEZA YA
                       </span>
                     </button>
@@ -20,7 +20,7 @@ const TecnologiasComponent = () => {
                 </div>
                 <div className='ct col-4 p-3'><p className=' '>Nos aseguramos de que tu experiencia sea completa, integrando nuestra aplicación con los mejores smartwatches del mercado. Monitoriza tu rendimiento físico, controla tu frecuencia cardíaca y recibe recomendaciones personalizadas sobre ejercicio y alimentación directamente en tu muñeca. Con nuestra tecnología, optimizar tu salud y bienestar está al alcance de tu mano.</p>
                   <div className='container-fluid d-flex justify-content-end'>
-                    <button>
+                    <button className='x'>
                       <span> EMPIEZA YA
                       </span>
                     </button>
